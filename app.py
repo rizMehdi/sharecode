c01624b63a44cfc0f4bfb167c1909bc5a9056933
rizMehdi <ccmeall@gmail.com>
2025-02-27 23:03:48 +0000

    hide text

--- app.py
+++ app.py
@@ -4,16 +4,6 @@ def main():
     # Create a container to manage visibility of content
     content_container = st.container()
     
-    with content_container:
-        st.title("IRESHA Sharecode")
-        st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
-
-        st.write("This webapp can be used to generate a sharecode indicating that you fulfil the minimum immigration/residence status eligibility requirements for social-housing assistance.")
-        
-        st.subheader("For applicants")
-        st.write("To create a sharecode, you will be asked a series of questions to check if you have the minimum eligibility for social-housing assistance.")
-        st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
-    
     if st.button("Check eligibility and create sharecode"):
         # Clear the previous content
         content_container.empty()
@@ -28,6 +18,16 @@ def main():
         ])
         
         st.write(f"You selected: {option}")
+    else:
+        with content_container:
+            st.title("IRESHA Sharecode")
+            st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
+
+            st.write("This webapp can be used to generate a sharecode indicating that you fulfil the minimum immigration/residence status eligibility requirements for social-housing assistance.")
+            
+            st.subheader("For applicants")
+            st.write("To create a sharecode, you will be asked a series of questions to check if you have the minimum eligibility for social-housing assistance.")
+            st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
         
 if __name__ == "__main__":
     main()
