335377afb7d57a9445e648372b52f6f55b19fc24
rizMehdi <ccmeall@gmail.com>
2025-02-27 22:55:13 +0000

    on click

--- app.py
+++ app.py
@@ -9,19 +9,20 @@ def main():
         st.title("IRESHA Sharecode")
         st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
 
-        styles = {'material-icons':{'color': 'red'},
-                  'text-icon-link-close-container': {'box-shadow': '#3896de 0px 4px'},
-                  'notification-text': {'':''},
-                  'close-button':{'':''},
-                  'link':{'':''}}
-        
-        custom_notification_box(icon='info', textDisplay='We are almost done with your registration...', externalLink='more info', url='#', styles=styles, key="foo")
-
         st.write("This webapp can be used to generate a sharecode indicating that you fulfil the minimum immigration/residence status eligibility requirements for social-housing assistance.")
         
         st.subheader("For applicants")
         st.write("To create a sharecode, you will be asked a series of questions to check if you have the minimum eligibility for social-housing assistance.")
         st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
+        
+        if st.button("?"):
+            styles = {'material-icons':{'color': 'black'},
+                      'text-icon-link-close-container': {'box-shadow': '#3896de 0px 4px'},
+                      'notification-text': {'':''},
+                      'close-button':{'':''},
+                      'link':{'':''}}
+            
+            custom_notification_box(icon='info', textDisplay='We are almost done with your registration...', externalLink='more info', url='#', styles=styles, key="foo")
     
     if st.button("Check eligibility and create sharecode"):
         # Clear the previous content
@@ -37,7 +38,6 @@ def main():
         ])
         
         st.write(f"You selected: {option}")
-        
         
 if __name__ == "__main__":
     main()
