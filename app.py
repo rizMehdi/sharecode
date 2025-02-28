2621a66785a9dd9e84089ce6abc74229ef47aead
rizMehdi <ccmeall@gmail.com>
2025-02-28 20:50:52 +0000

    remove container

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import streamlit_survey as ss
 
 def main():
-    # Create a container to manage visibility of content
-    content_container = st.container()
-    
     # Initialize the survey
     survey = ss.StreamlitSurvey("Survey Example 2 - Advanced Usage")
     pages = survey.pages(2, on_submit=lambda: st.json(survey.to_json()))
@@ -15,19 +12,18 @@ def main():
 
     with pages:
         if pages.current == 0:
-            with content_container:
-                st.title("IRESHA Sharecode")
-                st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
+            st.title("IRESHA Sharecode")
+            st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
 
-                st.write("This webapp can be used to generate a sharecode indicating that you fulfil the minimum immigration/residence status eligibility requirements for social-housing assistance.")
-                
-                st.subheader("For applicants")
-                st.write("To create a sharecode, you will be asked a series of questions to check if you have the minimum eligibility for social-housing assistance.")
-                st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
-                
-                if st.button("Check eligibility and create sharecode"):
-                    st.session_state['button_clicked'] = True
-                    st.rerun()
+            st.write("This webapp can be used to generate a sharecode indicating that you fulfil the minimum immigration/residence status eligibility requirements for social-housing assistance.")
+            
+            st.subheader("For applicants")
+            st.write("To create a sharecode, you will be asked a series of questions to check if you have the minimum eligibility for social-housing assistance.")
+            st.write("There might be additional requirements based on where you live or where you want to apply for social housing.")
+            
+            if st.button("Check eligibility and create sharecode"):
+                st.session_state['button_clicked'] = True
+                st.rerun()
         if pages.current == 1:
             st.write("Are you any of the following?")
             selected_page0 = survey.radio(
