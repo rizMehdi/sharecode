3fb035859705ec263e07a1bbc9fb314e13d13c17
rizMehdi <ccmeall@gmail.com>
2025-02-28 20:52:56 +0000

    add

--- app.py
+++ app.py
@@ -2,16 +2,48 @@ import streamlit as st
 import streamlit_survey as ss
 
 def main():
-    # Initialize the survey
-    survey = ss.StreamlitSurvey("Survey Example 2 - Advanced Usage")
-    pages = survey.pages(2, on_submit=lambda: st.json(survey.to_json()))
+    # Create a container to manage visibility of content
+    content_container = st.container()
+    
+    if st.session_state.get('button_clicked', False):
+        # Clear the previous content
+        content_container.empty()
+        
+        # Initialize the survey
+        survey = ss.StreamlitSurvey("Survey Example 2 - Advanced Usage")
+        pages = survey.pages(2, on_submit=lambda: st.json(survey.to_json()))
 
-    # Button customization
-    pages.prev_button = pages.default_btn_previous("Back")
-    pages.next_button = pages.default_btn_next("Next")
+        # Button customization
+        # pages.submit_button = pages.default_btn_submit("Soumettre")
+        pages.prev_button = pages.default_btn_previous("Back")
+        pages.next_button = pages.default_btn_next("Next")
 
-    with pages:
-        if pages.current == 0:
+        with pages:
+            if pages.current == 1:
+                st.write("Are you any of the following?")
+                Selected_page0= survey.radio(
+                    "used_st_before",
+                    options=[
+                        "British Citizen",
+                        "Irish Citizen",
+                        "Commonwealth Citizen (?)",
+                        "Diplomat or their family member based in the UK",
+                        "None of the above"],
+                    index=0,
+                    label_visibility="collapsed",
+                    horizontal=False,
+                )
+
+
+            if pages.current == 1:
+                st.write("This is the last question.")
+                acknowledge = survey.checkbox("Acknowledge")
+        
+        # if st.button("Back"):
+        #     st.session_state['button_clicked'] = False
+        #     st.rerun()
+    else:
+        with content_container:
             st.title("IRESHA Sharecode")
             st.header("Immigration/Residence Status Eligibility for Social Housing Assistance - Sharecode")
 
@@ -24,40 +56,6 @@ def main():
             if st.button("Check eligibility and create sharecode"):
                 st.session_state['button_clicked'] = True
                 st.rerun()
-        if pages.current == 1:
-            st.write("Are you any of the following?")
-            selected_page0 = survey.radio(
-                "used_st_before",
-                options=[
-                    "British Citizen",
-                    "Irish Citizen",
-                    "Commonwealth Citizen",
-                    "Diplomat or their family member based in the UK",
-                    "None of the above"
-                ],
-                index=0,
-                label_visibility="collapsed",
-                horizontal=False,
-            )
-
-            if selected_page0 == "British Citizen":
-                location = survey.radio(
-                    "current_location",
-                    options=[
-                        "UK",
-                        "Republic of Ireland",
-                        "Isle of Man",
-                        "Channel Islands",
-                        "None of the Above"
-                    ],
-                    index=0,
-                    label_visibility="collapsed",
-                    horizontal=False,
-                )
-                st.write(f"You selected: {location}")
-
-            st.write("This is the last question.")
-            acknowledge = survey.checkbox("Acknowledge")
         
 if __name__ == "__main__":
     main()
