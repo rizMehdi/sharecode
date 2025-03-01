9cd828ce1eee4bf3c561d1dea23ac077e9129b30
rizMehdi <ccmeall@gmail.com>
2025-03-01 09:17:26 +0000

    ds

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def main():
         st.markdown("""
         <style>
                .block-container {
-                    padding-top: 0rem;
+                    padding-top: 0.5rem;
                     padding-bottom: 0rem;
                     padding-left: 1rem;
                     padding-right: 1rem;
