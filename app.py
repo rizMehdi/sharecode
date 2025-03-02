da454cf1a793c02c59384ce420fbe00b1065427e
rizMehdi <ccmeall@gmail.com>
2025-03-02 14:27:18 +0000

    verbose false

--- app.py
+++ app.py
@@ -216,8 +216,8 @@ def show(result):
 def main():
     st.session_state['prevAnswer']="None"
     st.logo("img/IRESHAwide.png")
-    # verbose=False
-    verbose=True
+    verbose=False
+    # verbose=True
     if verbose: st.write(st.session_state.get('current_page', "start"))
     if verbose: st.write(st.session_state.get('prevAnswer', "None"))
     # Create a container to manage visibility of content
