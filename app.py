f9c986e8c5d27b75e1ebe266f85d641ed124ef08
rizMehdi <ccmeall@gmail.com>
2025-03-02 10:14:21 +0000

    remove heading

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def show(result):
     elif result == "result3":
         st.info(
             """
+            #### 
             You do not meet the minimum requirements to be eligible for housing assistance. 
             ---  
 
